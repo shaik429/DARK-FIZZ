fdc3e4216babd882281fe76d205305b82186146e
Dheekshith-zER0 <dheekshith.m.07@gmail.com>
2026-09-26 14:47:00 +0530

    chore: app and main jsx updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import AppLayout from "./layouts/AppLayout";
+import ProtectedRoute from "./components/ProtectedRoute";
+import ServerDownBanner from "./components/ServerDownBanner";
+import ToastHost from "./components/Toast";
 
 import AuthPage from "./pages/AuthPage";
 import DashboardPage from "./pages/DashboardPage";
@@ -11,17 +14,21 @@ import SettingsPage from "./pages/SettingsPage";
 export default function App() {
   return (
     <BrowserRouter>
+      <ServerDownBanner />
+      <ToastHost />
       <Routes>
         <Route path="/login" element={<AuthPage />} />
-
-        <Route element={<AppLayout />}>
-          <Route path="/" element={<DashboardPage />} />
-          <Route path="/products" element={<ProductsPage />} />
-          <Route path="/operations" element={<OperationsPage />} />
-          <Route path="/operations/new" element={<OperationFormPage />} />
-          <Route path="/operations/:id" element={<OperationFormPage />} />
-          <Route path="/settings" element={<SettingsPage />} />
+        <Route element={<ProtectedRoute />}>
+          <Route element={<AppLayout />}>
+            <Route path="/" element={<DashboardPage />} />
+            <Route path="/products" element={<ProductsPage />} />
+            <Route path="/operations" element={<OperationsPage />} />
+            <Route path="/operations/new" element={<OperationFormPage />} />
+            <Route path="/operations/:id" element={<OperationFormPage />} />
+            <Route path="/settings" element={<SettingsPage />} />
+          </Route>
         </Route>
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
